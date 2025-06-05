97416f4c4ecac197f566597c54515e8cdc8c1677
MR-MoRRiSoN <84626870+MR-MoRRiSoN@users.noreply.github.com>
2025-06-05 13:12:05 +0400

    add_spring_security

---
+++ src/main/java/com/morrison/vpnmanager/ApplicationController.java
@@ -0,0 +1,14 @@
+package com.morrison.vpnmanager;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hi")
+public class ApplicationController {
+    @GetMapping
+    private String hi() {
+        return "Hi, I'm RemotePlay VPN Manager";
+    }
+}
---
+++ src/main/java/com/morrison/vpnmanager/UserConfig.java
@@ -0,0 +1,36 @@
+package com.morrison.vpnmanager;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfig {
+
+    @Value("${spring.application.app-manager-user}")
+    private String username;
+    @Value("${spring.application.app-manager-passwd}")
+    private String password;
+    @Value("${spring.application.app-manager-role}")
+    private String role;
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername(username)
+                        .password(passwordEncoder.encode(password))
+                        .roles(role)
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
