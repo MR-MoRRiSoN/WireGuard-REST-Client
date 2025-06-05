61a31f11879d739c4ce632b77dc07680ec60476d
MR-MoRRiSoN <84626870+MR-MoRRiSoN@users.noreply.github.com>
2025-06-05 12:45:37 +0400

    uppload_vpn_manager_project

---
+++ src/test/java/com/morrison/vpnmanager/VpnManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrison.vpnmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
